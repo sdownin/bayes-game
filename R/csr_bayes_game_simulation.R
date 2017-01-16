7f12871a5db167a6f4e782585d10eaaffb5c01d0
Stephen Downing <stephendowning2008@gmail.com>
2017-01-16 14:39:14 +0800

    labes variables in bayes-game config object

--- R/csr_bayes_game_simulation.R
+++ R/csr_bayes_game_simulation.R
@@ -12,7 +12,9 @@ library(stringr)
 library(foreach)
 library(doParallel)
 
-#  load(file = "l_list_7epsilons_only3finished.RData")
+## NOTE: if missing package, run:
+## install.packages(c('packageName1','packageName2')) ## for N packages
+
 
 #--------------------------------------------------------------------------------------
 ##  RUN MAIN GAME SIMULATION
@@ -25,43 +27,46 @@ Tau <- 10 # 1003                   # number of periods
 
 ## GAME CONFIG
 x <- list(t=1
-          , q= .5           # focal parameter
-          , epsilon = 1    # focal parameter
-          , params=c('q')
-          , J1.0=200, J2.0=50  # secondary focal param
-          #, p1.0=1, p2.0=1
-          , v1= 1, v2=1
-          , omega=2  ## ****
-          , db1=.1, db2=.1          ## (db1)% buy all (y/pk) goods from current platform 2; (1-db1)% defect to multihome buying s1*(y/p1) from Plat 1, s2*(y/p2) from Plat 2
-          , dj1=.1, dj2=.1
-          , c1=.5, c2=.5            ## seller MARGINAL cost
-          , gamma1=.2, gamma2=.2  ## seller CSR cost
-          , phi1 =.4, phi2 =.4
-          , w1=.02, w2=.02          ## Platform operator MARGINAL cost
-          , psi1=.02, psi2=.02      ## Platform operator CSR cost   moved --> function of (gamma, B, y, p1)
-          , a1=1, a2=1
-          , r1=.1, r2=.1
-          , growth=.01
-          , Y=100
-          , ep=1
-          , Tau=Tau
-          , probs=c(.005,.025,.5,.975,.995)
-          , learningThreshold=.05
-          , n.iter=100
-          , sig1=c(rep(0,t1.change.pd),rep(1,Tau-t1.change.pd))
-          , sig2=c(rep(0,t2.change.pd),rep(1,Tau-t2.change.pd))   #rep(1,Tau)
-          , t1.change=t1.change.pd, t2.change=t2.change.pd
-          , cl.cutoff=0.7   # clustering between SS cutoff for 'learning' q
-          , parallel = TRUE  
-          , method = 'rjparallel'
-          , n.cores = 4
-          , learn = FALSE
-          , Gdist = 'binomial'
+          , q= .5              ## hedonic probability by nature (proportion)
+          , epsilon = 1        ## indirect network effects
+          , params=c('q')      ## parmeters to be 'learned' by gibbs sampling
+          , J1.0=200, J2.0=50  ## Initial sellers on platforms
+          #, p1.0=1, p2.0=1    ## price  *MOVED* to be set dynamically as function of CSR policy
+          , v1= 1, v2=1        ## utilitarian value
+          , omega=2  ##        ## hedonic value
+          , db1=.1, db2=.1     ## buyer churn:  (db1)% buy all (quantity = y/pk) goods from current platform 2; (1-db1)% defect to multihome buying s1*(y/p1) from Plat 1, s2*(y/p2) from Plat 2
+          , dj1=.1, dj2=.1     ## seller churn
+          , c1=.5, c2=.5       ## seller MARGINAL cost
+          , gamma1=.2, gamma2=.2  ## seller CSR cost 
+          , phi1 =.4, phi2 =.4    ## platfom CSR cost
+          , w1=.02, w2=.02        ## Platform operator MARGINAL cost
+          # , psi1=.02, psi2=.02  ## Platform operator CSR cost   *MOVED* --> function of (gamma, B, y, p1)
+          , a1=1, a2=1        ## time constant
+          , r1=.1, r2=.1      ## platform transaction fee (percent of price)
+          , growth=.01        ## ** currently not using 
+          , Y=100             ## budget
+          , ep=1              ## downweighted below
+          , Tau=Tau           ## number of periods in simulation
+          , probs=c(.005,.025,.5,.975,.995)  ## quantiles of estimated parameter to keep
+          , learningThreshold=.05            ## ** don't need
+          , n.iter=100                       ## ** don't need
+          , sig1=c(rep(0,t1.change.pd),rep(1,Tau-t1.change.pd))   ## ** don't need
+          , sig2=c(rep(0,t2.change.pd),rep(1,Tau-t2.change.pd))   ## ** don't need # #rep(1,Tau)
+          , t1.change=t1.change.pd, t2.change=t2.change.pd    ## ** don't need
+          , cl.cutoff=0.7         ## ** don't need # clustering between SS cutoff for 'learning' q
+          , parallel = TRUE       ## ** don't need
+          , method = 'rjparallel' ## ** don't need
+          , n.cores = 4           ## ** don't need
+          , learn = FALSE         ## ** don't need
+          , Gdist = 'binomial'    ## ** don't need
 )
 #  learn methods:  'rjags', 'simple', 'interruptible', 'parallel', 'rjparallel', 
 #                  'background', 'bgparallel' or 'snow'
 
-#----------------- Multi-Parm Simulation  ----------------------------
+#---------------------------------------------------------------------
+#----------------- Multi-Param Simulation  ---------------------------
+#---------------------------------------------------------------------
+
 qs <- c(0,.2,.4,.6,.8,1)   #seq(0,1,.1)
 epsilons <- c(.3,1.05,1.8)  #
 dbs <-  c(0.05, 0.1, 0.2) #c(.05,.5)
@@ -82,23 +87,30 @@ for (i in 1:length(qs)) {
   }
 }
 
-# ## UNCOMMENT to SAVE binary (.RData) data file  
+# ## UNCOMMENT to SAVE binary (.RData) data file
+# ## load saved binary file as follows:
+# ## load('filename.RData')
 # image.file <- sprintf('l_list_facet_plot_T_%s_w_%s_J1_%s_J2_%s_db_%s_q_%s.RData',x$Tau,x$omega,x$J1.0,x$J2.0,paste(dbs,collapse="-"),paste(qs,collapse="-"))
 # save.image(image.file)
 
 
+
 ##--------- PLOT BUYER SHARE -------------------------
+
+## subset data to plot
 df <- getBasePlotDf(l.list, id.vars=c('q','epsilon','db','phi','period'))
 db_i <- "0.2"   # 0.05, 0.1, 0.2
 phi_i <- c("0.02","0.1","0.5")
 df <- subset(df, subset=(db %in% db_i & phi %in% phi_i))
 
+## prepare plotting arguments
 colourCount = length(unique(df$q))
 db_num <- as.numeric(db_i)
 db_lab <- ifelse(db_num < 0.1, 'Low',ifelse(db_num < 0.2, 'Moderate','High'))
 # getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 gglty <- rep(c(2,1), length(unique(df$phi))*length(unique(df$epsilon)) )
 
+## GGPLOT object
 gg <- ggplot(aes(x=period, y=value, colour=q), data=df) + 
   scale_x_log10() +
   geom_line(aes(colour=q,group=q,lty=q), lwd=1.1) + 
@@ -108,10 +120,12 @@ gg <- ggplot(aes(x=period, y=value, colour=q), data=df) +
   geom_vline(xintercept=c(l.list[[1]]$t1.change+1,l.list[[1]]$t2.change+1),lty=gglty) +
   ggtitle(sprintf('%s Churn (%.2f)',db_lab,db_num)) + 
   ylab("Buyer Share") + xlab('Period') + theme_bw()
-gg
+gg  ## display plot
 
+## save plot
 file.name <- sprintf("buyer_base_share_PRICE_not_log_J1_%s_J2_%s_t1_%s_t2_%s_T%s_db_%s_omega_%s.png", x$J1.0, x$J2.0,x$t1.change,x$t2.change,x$Tau,db_i,x$omega)
 ggsave(file.name, gg, height=8, width=12, units='in')
+
 
 
 
