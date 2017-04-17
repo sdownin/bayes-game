8fdad2eb4a018cf98df22874bb3682e17e788bf9
Stephen Downing <stephendowning2008@gmail.com>
2017-04-18 04:21:14 +0800

    fixes changes to simulation response period plot

--- R/csr_bayes_game_simulation_time_threshold.R
+++ R/csr_bayes_game_simulation_time_threshold.R
@@ -27,7 +27,7 @@ library(doParallel)
 ## Set strategy change periods and total simulation length
 t1.change.pd <- 100            # platform 1 adds CSR policy at period
 t2.change.pd <- 2            # platform 2 adds CSR policy at period
-Tau <- 2000                   # number of periods
+Tau <- 1400                   # number of periods
 
 ## GAME CONFIG
 x <- list(t=1
@@ -72,11 +72,11 @@ x <- list(t=1
 
 l.list <- list()   #testing:#    i = j = k = l = m = 1
 
-qs <- seq(0,1,.1) ##c(0,.02,.05,.1,.2,.3,.4) # c(0,.2,.4,.6,.8,1)  #seq(0,1,.1)
-epsilons <- c(.5,1.05,1.6)  #
+qs <- c(0, .2, .4, .6, .8, 1)  # seq(0,1,.1) ##c(0,.02,.05,.1,.2,.3,.4) # c(0,.2,.4,.6,.8,1)  #seq(0,1,.1)
+epsilons <- c(.3, 1.05, 1.8)  #
 dbs <-  0.1 # c(0.05, 0.1, 0.2) #c(.05,.5)
 phis <- 0.1  # c(0.02, 0.1, 0.5)   # CSR cost-base price increase
-t1.changes <- c(10, 30, 90) 
+t1.changes <- c(30, 90, 270) # c(20, 80, 320) 
 
 for (i in 1:length(qs)) {
   for (j in 1:length(epsilons)) {
@@ -116,13 +116,22 @@ save(l.list, file=image.file)
 ## choose params to display
 db_i <- "0.1"   # "0.05", "0.1", "0.2"
 phi_i <- "0.1" # c("0.02","0.1","0.5")
-q_i <- c(0,.02,.05,.1,.2,.3)  # .4
-t1.change_i <- c(10, 30, 90)
+q_i <- c('0', '0.2', '0.4', '0.6', '0.8', '1')  # .4
+t1.change_i <- t1.changes#c(20, 60, 180)
+
+
 ## subset data by chosen params
 df <- getBasePlotDf(l.list, id.vars=c(names(l.list[[1]]$params), 'period') )
-df <- subset(df, subset=(db %in% db_i & phi %in% phi_i & t1.change %in% t1.change_i & q %in% q_i))
+df <- subset(df, subset=(db %in% db_i & phi %in% phi_i & t1.change %in% t1.change_i))
+df <- subset(df, subset=(as.character(q) %in% q_i))  ## couldn't filter 0.6 as float ?? need to use characters
 
 ## set numerics to factors for plotting
+periods <- c('Early','Mid','Late')
+df$t1.change <- sapply(seq_len(nrow(df)), function(i){
+  if (df$t1.change[i]==t1.changes[1]) return( sprintf('1. Early Response = %s',df$t1.change[i]) )
+  if (df$t1.change[i]==t1.changes[2]) return( sprintf('2. Mid Response = %s',df$t1.change[i]) )
+  if (df$t1.change[i]==t1.changes[3]) return( sprintf('3. Late Response = %s',df$t1.change[i]) )
+});
 for(var in names(l.list[[1]]$params))
   df[,var] <- factor(df[,var], levels = as.character(sort(unique(df[,var]))))
 
@@ -137,8 +146,8 @@ if (length(unique(df$period))>40) {
 nPeriods <- length(unique(df$period))
 ncols <- length(epsilons)
 xintercepts2 <- l.list[[1]]$t2.change+1
-xintercepts1 <- data.frame(z = rep(as.numeric(levels(df$t1.change)), each=ncols), 
-                           t1.change = rep(t1.change_i, each=ncols), 
+xintercepts1 <- data.frame(z = rep(t1.change_i, each=ncols), 
+                           t1.change = rep(levels(df$t1.change), each=ncols), 
                            epsilon = rep(epsilons, ncols) )
 colourCount <- length(unique(df$q))
 db_num <- as.numeric(db_i)
@@ -146,35 +155,16 @@ db_lab <- ifelse(db_num < 0.1, 'Low',ifelse(db_num < 0.2, 'Moderate','High'))
 # getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 
 # make labeller
-labels.epsilon = sapply(epsilons, function(x)sprintf("Network Effect = %3s",x))
-labels.t1.change = sapply(t1.change_i, function(x)sprintf("Response Pd = %3s",x))
+neLevels <- c('Low','Moderate','High')
+# periods <- c('Early','Mid','Late')
+labels.epsilon = sapply(seq_along(epsilons), function(x)sprintf("%s Network Effect = %3s",neLevels[x],epsilons[x]))
+labels.t1.change = sapply(seq_along(levels(df$t1.change)), function(x)levels(df$t1.change)[x])  # sapply(seq_along(t1.change_i), function(x)sprintf("%s. %s Response = %s",x,periods[x],t1.change_i[x]))
 names(labels.epsilon) <- epsilons
-names(labels.t1.change) <- t1.change_i
+names(labels.t1.change) <- levels(df$t1.change)
 labels <- c(labels.epsilon, labels.t1.change)
 
 # legend guide
 legend.guide <- guide_legend(title="Hedonic\nProportion\n(q)")
-
-## GGPLOT object
-gg <- ggplot(aes(x=period, y=value, colour=q), data=df) + 
-  geom_line(aes(colour=q,group=q,lty=q), lwd=1.1) + 
-  facet_grid(t1.change ~ epsilon, labeller = as_labeller(labels)) + 
-  geom_point(aes(pch=q), data=df.point) + 
-  scale_color_manual(values=colorRamps::matlab.like(colourCount)) +
-  geom_vline(xintercept=xintercepts2, lty=1) +
-  geom_vline(aes(xintercept=z), xintercepts1, lty=2) +
-  scale_x_log10() + ylim(0,1) +
-  ggtitle(sprintf('%s Churn (%.2f)',db_lab,db_num)) + 
-  ylab("Buyer Share") + xlab(expression('Time Period ('*log[10]~scale*')')) + 
-  guides(color=legend.guide, group=legend.guide,lty=legend.guide,pch=legend.guide) +
-  theme_bw() 
-gg  ## display plot
-
-## save plot
-file.name <- sprintf("buyer_base_share_PERIOD_not_log_J1_%s_J2_%s_t1_%s_t2_%s_T%s_db_%s_phi_%s_omega_%s.png", x$J1.0, x$J2.0,paste(x$t1.change,collapse = "-"),x$t2.change,x$Tau,db_i,phi_i,x$omega)
-ggsave(file.name, gg, height=8, width=12, units='in')
-
-
 
 ##----------- No title figure for article ----------------------------
 ## GGPLOT object
@@ -196,6 +186,26 @@ file.name <- sprintf("_uber_buyer_base_share_PERIOD_6p5-10_not_log_J1_%s_J2_%s_t
 ggsave(file.name, gg, height=6.5, width=10, units='in')
 ##------------------------------------------------------------
 
+
+##-----------------with title--------------------------------
+## GGPLOT object
+gg <- ggplot(aes(x=period, y=value, colour=q), data=df) + 
+  geom_line(aes(colour=q,group=q,lty=q), lwd=1.1) + 
+  facet_grid(t1.change ~ epsilon, labeller = as_labeller(labels)) + 
+  geom_point(aes(pch=q), data=df.point) + 
+  scale_color_manual(values=colorRamps::matlab.like(colourCount)) +
+  geom_vline(xintercept=xintercepts2, lty=1) +
+  geom_vline(aes(xintercept=z), xintercepts1, lty=2) +
+  scale_x_log10() + ylim(0,1) +
+  ggtitle(sprintf('%s Churn (%.2f)',db_lab,db_num)) + 
+  ylab("Buyer Share") + xlab(expression('Time Period ('*log[10]~scale*')')) + 
+  guides(color=legend.guide, group=legend.guide,lty=legend.guide,pch=legend.guide) +
+  theme_bw() 
+gg  ## display plot
+
+## save plot
+file.name <- sprintf("buyer_base_share_PERIOD_not_log_J1_%s_J2_%s_t1_%s_t2_%s_T%s_db_%s_phi_%s_omega_%s.png", x$J1.0, x$J2.0,paste(x$t1.change,collapse = "-"),x$t2.change,x$Tau,db_i,phi_i,x$omega)
+ggsave(file.name, gg, height=8, width=12, units='in')
 
 
 
